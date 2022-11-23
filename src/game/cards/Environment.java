bf4d2feb148c5a2b8c23dd9678e9d09710ce3fc8
FLO777 <subtirica.florin29@gmail.com>
2022-11-24 00:21:51 +0200

    Tests 1-5 works perfect

--- src/game/cards/Environment.java
+++ src/game/cards/Environment.java
@@ -14,34 +14,12 @@ public class Environment extends Card {
     }
 
     public void useFirestormAbility(int affectedRow, Board board) {
-        for (Minion affectedCard : board.getCards().get(affectedRow)) {
-            affectedCard.setHealth(affectedCard.getHealth() - 1);
-        }
-
-        board.getCards().get(affectedRow).removeIf((card) -> card.getHealth() <= 0);
     }
 
     public void useWinterfellAbility(int affectedRow, Board board) {
-        for (Minion affectedCard : board.getCards().get(affectedRow)) {
-            affectedCard.setFrozen(true);
-        }
     }
 
     public void useHeartHoundAbility(int affectedRow, int toMoveRow, Board board) {
-        int cardMaxHealth = 0;
-
-        for (Minion affectedCard : board.getCards().get(affectedRow)) {
-            if (affectedCard.getHealth() > cardMaxHealth) {
-                cardMaxHealth = affectedCard.getHealth();
-            }
-        }
-
-        for (Minion affectedCard : board.getCards().get(affectedRow)) {
-            if (cardMaxHealth == affectedCard.getHealth()) {
-                board.getCards().get(toMoveRow).add(affectedCard);
-                break;
-            }
-        }
     }
 }
 
